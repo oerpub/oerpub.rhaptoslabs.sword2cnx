4b17f4b1e2fb2fd584cdc86418ada1f3769fdacb
Johan Beyers <jbeyers@juizi.com>
2011-10-05 15:10:11 +0200

    Reorganised files to match package

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='oerpub.rhaptoslabs.sword2cnx',
+      version=version,
+      description="A library that implements the SWORD2 API specification to communicate with Connexions (cnx.org), a Rhaptos instance.",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Carl Scheffler',
+      author_email='carl.scheffler@gmail.com',
+      url='https://github.com/cscheffler/oerpub.rhaptoslabs.sword2cnx',
+      license='GPLv3',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['oerpub', 'oerpub.rhaptoslabs'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
